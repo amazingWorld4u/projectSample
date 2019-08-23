f14a55985da29a0aab8a16b9a7ca6eac87da4dfb
amazingWorld4u <47545952+amazingWorld4u@users.noreply.github.com>
2019-08-23 05:14:59 +0000

    Update App.js

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ class App extends Component{
   render(){
     return(
       <View>
-        <Text>hello</Text>
+        <Text>Good</Text>
         <TouchableHighlight onPress={() => {
   var options = {
     description: 'Credits towards consultation',
